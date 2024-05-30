f17add22e3c748f91f4c3cde25b4751434420e8c
Alex03198 <aleksandrsubbota0@gmail.com>
2024-05-31 03:56:09 +1000

    Add files via upload

---
+++ homework2(1).py
@@ -0,0 +1,12 @@
+The_number_of_completed_DZ=12  #Количество выполненных ДЗ
+Number_of_hours_spent=1.5  #Количество затраченных часов
+Course_name="Python"  #Название курса
+Time_for_one_task=(Number_of_hours_spent/The_number_of_completed_DZ)  #Время на одно задание
+e=", всего задач:"
+f=", затрачено часов:"
+g=", среднее время выполнения"
+h="часа."
+
+print(Course_name,e,The_number_of_completed_DZ,f,Number_of_hours_spent,g,Time_for_one_task,h)
+
+print(Course_name,", всего задач:",The_number_of_completed_DZ,", затрачено часов:",Number_of_hours_spent,", среднее время выполнения",Time_for_one_task,"часа.")
